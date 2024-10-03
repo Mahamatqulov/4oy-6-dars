6a508dbf400ebb72d4a3d6a8f76fc85960e09194
bahrom <baxrom@email.com>
2024-10-03 17:51:28 +0500

    js 6-dars ready

---
+++ app.js
@@ -0,0 +1,221 @@
+// 1-misol
+// function yigindi(a, b) {
+//   return a + b;
+// }
+
+// let natija = yigindi(5, 7);
+// console.log(natija);
+
+//2-misol
+// function minutsekundga(minut) {
+//   return minut * 60;
+// }
+// let sekund = minutsekundga(5);
+// console.log(sekund);
+
+//3-misol
+// function songabirqosh(son) {
+//   return son + 1;
+// }
+// let sonlar = songabirqosh(3);
+// console.log(sonlar);
+
+//4-misol
+// function uchburchakYuzi(asos, baland) {
+//   return (asos * balandlik) / 2;
+// }
+// let asos = 5;
+// let balandlik = 10;
+// let natija = uchburchakYuzi(asos, balandlik);
+// console.log(natija);
+
+//5-misol
+// // function minutsekundga(minut) {
+//   return minut * 60;
+// }
+// let sekund = minutsekundga(5);
+// console.log(sekund);
+
+//6-misol
+// function sonningKubi(son) {
+//   return son ** 3;
+// }
+// let natija = sonningKubi(3);
+// console.log(natija);
+
+//7-misol
+// function birinchiElement(massiv) {
+//   return massiv[0];
+// }
+// let massivlar = [10, 20, 30, 40];
+// let natija = birinchiElement(massivlar);
+// console.log(natija);
+
+//8-misol
+// function power(x, y) {
+//   return x * y;
+// }
+
+// let natija = power(5, 3);
+// console.log(natija);
+
+//9-misol
+// function soatsekundga(soat) {
+//   return soat * 3600;
+// }
+// let natija = soatsekundga(2);
+// console.log(natija);
+
+//10-misol
+// function uchburchakTomon(tomon1, tomon2) {
+//   return tomon1 + tomon2;
+// }
+// let tomon1 = 4;
+// let tomon2 = 6;
+// let natija = uchburchakTomon(tomon1, tomon2);
+// console.log(natija);
+
+//11-misol
+///////
+
+//12-misol
+// function turtburchakYuzi(boyi, eni) {
+//   return boyi * eni;
+// }
+
+// let boyi = 6;
+// let eni = 4;
+// let natija = turtburchakYuzi(6, 4);
+// console.log(natija);
+
+//13-misol
+// function stringQoshish(a) {
+//   return "something " + a;
+// }
+
+// console.log(stringQoshish("is better than nothing"));
+// console.log(stringQoshish("Bob Jane"));
+// console.log(stringQoshish("something"));
+
+//14-misol
+// function sonniKvadrati(son) {
+//   return son ** 2;
+// }
+// let son = prompt("son kiriting");
+// let natija = sonniKvadrati(son);
+// console.log(natija);
+
+//15-misol
+// function noldan(son) {
+//   if (son < 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// let kiritilganSon = prompt("Son kiriting:");
+// kiritilganSon = Number(kiritilganSon);
+// console.log(noldan(kiritilganSon));
+
+//16-misol
+// function ichkiBurchaklar(n) {
+//   return (n - 2) * 180;
+// }
+// let n = prompt("n sonini kiriting");
+// let natija = ichkiBurchaklar(n);
+// console.log(natija);
+
+//17-misol
+// function ochkolar(x, y) {
+//   return x * 2 + y * 3;
+// }
+
+// let x = prompt("2 ochkoli zonadan kiritilgan top");
+// let y = prompt("3 ochkoli zonadan kiritilgan top");
+// let natija = ochkolar(x, y);
+// console.log(natija + " ochko");
+
+//18-misol
+// function nameString(name) {
+//   return name + "Edabit";
+// }
+
+// let name = prompt("ism kiriting");
+// let natija = nameString(name);
+// console.log(natija);
+
+//19-misol
+// function kichik100dan(x, y) {
+//   return x + y < 100;
+// }
+
+// let x = prompt("x sonini kiriting");
+// let y = prompt("y sonini kiriting");
+// let natija = kichik100dan(x, y);
+// console.log(natija);
+
+//20-misol
+// function printArray(n) {
+//   const result = [];
+//   for (let i = 1; i <= n; i++) {
+//     result.push(i);
+//   }
+//   return result;
+// }
+// let n = prompt("n sonini kiriting");
+// let natija = printArray(n);
+// console.log(natija);
+
+//21-misol
+// function oyoqlar(tovuq, qoy, sigir) {
+//   return tovuq * 2 + qoy * 4 + sigir * 4;
+// }
+
+// let tovuq = prompt("tovuqlar soni");
+// let qoy = prompt("qoylar soni");
+// let sigir = prompt("sigirlar soni");
+// let natija = oyoqlar(tovuq, qoy, sigir);
+// console.log(natija + " ta hamma hayvonlarning oyoqlar soni");
+
+//22-misol
+// function and(a, b) {
+//   return a && b;
+// }
+
+// console.log(and(true, false));
+// console.log(and(true, true));
+// console.log(and(false, true));
+// console.log(and(false, false));
+
+//23-misol
+// function tengMi(x, y) {
+//   return x === y;
+// }
+// let x = prompt("x ga son kiriting");
+// let y = prompt("y ga son kiriting");
+// let natija = tengMi(x, y);
+// console.log(natija);
+
+//24-misol
+// function futbolOchko(yutishlar, duranglar, maglubiyatlar) {
+//   return yutishlar * 3 + duranglar * 1 + maglubiyatlar * 0;
+// }
+
+// let yutishlar = prompt(" g'alabalar soni");
+// let duranglar = prompt("duranglar soni");
+// let maglubiyatlar = prompt("maglubiyatlar soni");
+// let natija = futbolOchko(yutishlar, duranglar, maglubiyatlar);
+// console.log(natija);
+
+//25-misol
+// function sekundlar(soat, minut) {
+//   let soatSekund = soat * 3600;
+//   let minutSekund = minut * 60;
+
+//   return soatSekund + minutSekund;
+// }
+
+// let soat = prompt("soatni kiriting");
+// let minut = prompt("minutni kiriting");
+// let natija = sekundlar(soat, minut);
+// console.log(natija + " sekund");
